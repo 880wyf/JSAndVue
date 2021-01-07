7aaa7d828b518a80e6b96fee36c4f7afe1239883
wyf880 <1669788397@qq.com>
2021-01-07 22:15:41 +0800

    完善动画效果

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,103 @@
 $(function () {
   var $navigation = $("#navigation")
-  $(document).scroll(function(event){
-    $top = $(this).scrollTop()
+  // 滚动 显示与隐藏
+  $(window).scroll(function(event){
+    var $top = $(this).scrollTop()
     if($top >= 640){
-      $navigation.css({
-        display:"block",
+      $navigation.stop().animate({
         width:"36px",
-        height:"369px",
-        transition: "all 5s ease 5s"
-      })
-    }else if($top <= 640){
-      $navigation.css({
-        display:"none"
-      })
+        height:"368px"
+      },200)
+    }else if($top < 640){
+      $navigation.stop().animate({
+        width:"0px",
+        height:"0px"
+      },200)
     }
 
   })
-   
+  
+  // 鼠标 移入变色
+  var colors = ["#00ff2a", "#0c0c0c", "#fc1dde", "#00f7ff", "#7bff00", "#ff4800", "#4bb2f7", "#090a0a"]
 
+  var $listA = $(".nav-info a")
+  
+  $(window).scroll(function(){
+    console.log($("body, html").scrollTop())
+    var $top = $("body, html").scrollTop()
+    if($top >= 1650 && $top < 2310){   
+      $listA.css("background","#666666")   
+      $($listA[0]).css("background","#00ff2a")
+    }else if($top >= 2310 && $top < 3180){
+      $listA.css("background","#666666")
+      $($listA[1]).css("background","#0c0c0c")
+    }else if($top >= 3180 && $top < 3850){
+      $listA.css("background","#666666")
+      $($listA[2]).css("background","#fc1dde")
+    }else if($top >= 3850 && $top < 4650){
+      $listA.css("background","#666666")
+      $($listA[3]).css("background","#00f7ff")
+    }else if($top >= 4650 && $top < 5315){
+      $listA.css("background","#666666")
+      $($listA[4]).css("background","#7bff00")
+    }else if($top >= 5315 && $top < 6110){
+      $listA.css("background","#666666")
+      $($listA[5]).css("background","#ff4800")
+    }else if($top >= 6110 && $top < 6840){
+      $listA.css("background","#666666")
+      $($listA[6]).css("background","#4bb2f7")
+    }else if($top >= 6840 ){
+      $listA.css("background","#666666")
+      $($listA[7]).css("background","#090a0a")
+    }else if($top < 1650){
+      $listA.css("background","#666666")
+    }
+  })
 
- 
+  $listA.on("mouseover", function () {
+    var index = $(this).attr("data-index")
+    $(this).css("background-color",colors[index])
+  })
+  $listA.on("mouseout", function () { 
+    $(this).css("background-color", "#666666")
+  })
+
+  var $ids = [
+              "#mail-supermarket",
+              "#mail-guoji",
+              "#mail-rensheng",
+              "#mail-kuwang",
+              "#mail-jujia",
+              "#mail-aicao",
+              "#mail-waichu",
+              "#mail-like"
+            ]
+
+  // 鼠标 点击移动
+  $listA.on("click", function () {
+    
+    $("body, html").stop().animate({scrollTop:$($ids[$(this).attr("data-index")]).offset().top}, 400)
+  })
+  // 鼠标 点击置顶
+  $("#navigation>.retop").on("click", function () {
+    $("body, html").stop().animate({scrollTop:0}, 400)
+  })
+
+  // 设置顶部搜索框
+
+  var $disSearch = $("#dis-search")
+  $(window).scroll(function () {
+    console.log($("body, html").scrollTop())
+    if($("body, html").scrollTop() >= 800){
+      $disSearch.stop().animate({
+        height:"50px"
+      },100)
+    }else if($("body, html").scrollTop() < 800) {
+      $disSearch.stop().animate({
+        height:0
+      },100)
+    }
+    
+  })
+
 })
--- js/swing.js
+++ js/swing.js
@@ -1,13 +1,29 @@
 $(function() {
   var $timer = null 
-  var $left = $("#banner .mail-image").css("left").split('p')[0]
-  timer = setInterval(function(){
-    $left -= 1230
-    if($left <= 0){
-      $("#banner .mail-image").css("left",$left+"px").fadeTo("slow",.8).fadeTo("slow",1);
+  var image = document.querySelector(".mail-image")
+  var index = 0
+  $timer = setInterval(anima, 100); 
+
+  function anima () {
+    if(image.offsetLeft == -7380){
+      clearInterval($timer)
+      index = 0
+      image.style.left = 0
+      $timer = setInterval(anima, 100);
+    }else{
+      if(index >= 7){
+        index = 0
+      }
+      var left = -1230*index
+      var sept = (left-image.offsetLeft)/6
+      sept = sept>0 ? Math.ceil(sept) : Math.floor(sept);
+      image.style.left = image.offsetLeft+sept+"px"
+      if(image.offsetLeft == left){
+        index++
+      }
     }
-    if($left <= -6150){
-      $left = 1230
+    
     }
-  }, 2000);
+
+  
 })
