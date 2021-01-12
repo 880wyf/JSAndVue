156c89d31fae97733da7ffc928b933e4c43b3f18
wyf880 <1669788397@qq.com>
2021-01-12 11:54:25 +0800

    完善轮播图

--- js/swing.js
+++ js/swing.js
@@ -1,4 +1,11 @@
 ;$(function() {
+  $(".mail-image > div").each(function () {
+    $("#banner .mail-rectangle ul").append("<li></li>")
+  })
+
+  $("#banner .mail-rectangle ul li").last().remove()
+  $("#banner .mail-rectangle ul li").addClass('rectangle')
+  
   var $timer = null 
   var image = document.querySelector(".mail-image")
   var index = 0
@@ -8,24 +15,28 @@
     if(image.offsetLeft == -7380){
       clearInterval($timer)
       index = 0
-      image.style.left = 0
-      $timer = setInterval(anima, 100);
+      image.style.left = 0   
+      $timer = setInterval(anima, 100);     
     }else{
-      if(index >= 7){
-        index = 0
-      }
+      
       var left = -1230*index
       var sept = (left-image.offsetLeft)/6
       sept = sept>0 ? Math.ceil(sept) : Math.floor(sept);
       image.style.left = image.offsetLeft+sept+"px"
+      if(index != 0){
+        $("#banner .mail-rectangle ul li").removeClass('on')
+        $($("#banner .mail-rectangle ul li")[index]).addClass("on")
+      }
+
       if(image.offsetLeft == left){
         index++
+      }
+      if(index == 6){
+        $($("#banner .mail-rectangle ul li")[0]).addClass("on")
       }
     }
     
     }
-
-
 
   
 })
